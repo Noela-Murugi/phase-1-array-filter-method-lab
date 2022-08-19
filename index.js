d3aaf0e0d7e78d44ab948850f286a205f7e7ed19
Noela-Murugi <murugin254@gmail.com>
2022-08-19 14:19:46 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+const drivers = [
+    {
+        firstName:"Noela",
+        hometown: "Nairobi",
+    },
+    {
+        fistName: "Abby",
+        hometown: "Nakuru",
+    },
+    {
+        fistName: "Nate",
+        hometown: "Eldoret",
+    },
+    {
+        fistName:  "Paul",
+        hometown: "Mombasa",
+    }
+   ];
+
+function findMatching(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase() === name.toLowerCase()
+    });
+  };
+
+  function fuzzyMatch(drivers, sName) {
+    return drivers.filter(function(driver){
+        return driver.slice(0, sName.length).toLowerCase() === sName.toLowerCase()
+    });
+  };
+
+  function matchName(drivers, driverProperty) {
+    return drivers.filter(function(driver){
+        return driver.name.toLowerCase() === driverProperty.toLowerCase()
+    });
+  };
